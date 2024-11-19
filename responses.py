b5df77c69d07997ecc8f166323a746e83a6380dd
MFOzair <138257889+MFOzair@users.noreply.github.com>
2024-11-19 07:31:10 +0500

    responses.py

---
+++ responses.py
@@ -0,0 +1,105 @@
+response_a = """
+***************************************
+The {} has a gluten-free option, but it is not vegan.
+***************************************
+"""
+
+response_b = """
+***************************************
+We have a selection of sides to go along with the {}, including mashed potatoes and steamed vegetables.
+***************************************
+"""
+
+response_c = """
+***************************************
+{} includes habanero, so it is a bit spicy!
+***************************************
+"""
+
+response_d = """
+***************************************
+The {} is a customer favorite, known for its rich flavors.
+***************************************
+"""
+
+response_e = """
+***************************************
+Our {} is made fresh daily with locally sourced ingredients.
+***************************************
+"""
+
+response_f = """
+***************************************
+The {} pairs well with a glass of our house red wine.
+***************************************
+"""
+
+response_g = """
+***************************************
+If you like cheese, the {} is a must-try!
+***************************************
+"""
+
+response_h = """
+***************************************
+The {} is served with a side of our homemade salsa.
+***************************************
+"""
+
+response_i = """
+***************************************
+For a lighter option, try the {} with a side salad.
+***************************************
+"""
+
+response_j = """
+***************************************
+The {} is cooked to perfection with a hint of garlic.
+***************************************
+"""
+
+response_k = """
+***************************************
+Our chef recommends the {} for a delightful experience.
+***************************************
+"""
+
+response_l = """
+***************************************
+The {} is a perfect choice for those who love spicy food.
+***************************************
+"""
+
+response_m = """
+***************************************
+You can customize the {} with your choice of toppings.
+***************************************
+"""
+
+
+blank_spot = "food"
+menu = """
+***************************************
+        Welcome to El Sabor Mexicano!
+***************************************
+
+Here is our delicious menu:
+
+1. Tacos al Pastor
+2. Enchiladas Verdes
+3. Spicy Habanero Salsa
+4. Mole Poblan
+5. Fresh Guacamole
+6. Chiles Rellenos
+7. Queso Fundido
+8. Carnitas Tacos
+9. Grilled Fish Tacos
+10. Garlic Shrimp
+11. Chef's Special Fajitas
+12. Spicy Chicken Wings
+13. Build-Your-Own Burrito
+
+***************************************
+"""
+
+responses = [response_a, response_b, response_c]
